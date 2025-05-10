16c8f167b3f1268d9bcac9e903f87c1835ca5e98
huangdund <1106412975@qq.com>
2025-05-11 00:44:26 +0800

    fix threadpool bug

--- serve/threadpool.h
+++ serve/threadpool.h
@@ -36,7 +36,24 @@ private:
 
 inline ThreadPool::ThreadPool(size_t threads, std::vector<std::string> &connections, Logger &lg) : stop(false) {
     for (size_t i = 0; i < threads; ++i) {
-        workers.emplace_back([this] {
+        workers.emplace_back([this, &connections, &lg] {
+            // Initialize thread-local storage for connections
+            for(size_t i = 0; i < connections.size(); ++i) {
+                std::string conninfo = connections[i];
+                try {
+                    // Create a connection for each thread
+                    pqxx::connection *conn = new pqxx::connection(conninfo);
+                    if (!conn->is_open()) {
+                        lg.error(" Failed to connect to the database. conninfo" + conninfo);
+                    } else {
+                        lg.info("Connected to the database successful.");
+                    }
+                    // Store the connection in thread-local storage
+                    connections_thread_local.push_back(conn);
+                } catch (const std::exception &e) {
+                    lg.error("Error while connecting to KingBase: " + std::string(e.what()));
+                }
+            }
             while (true) {
                 std::function<void()> task; {
                     std::unique_lock<std::mutex> lock(this->queue_mutex);
@@ -48,22 +65,6 @@ inline ThreadPool::ThreadPool(size_t threads, std::vector<std::string> &connecti
                 task();
             }
         });
-        for(size_t i = 0; i < connections.size(); ++i) {
-            std::string conninfo = connections[i];
-            try {
-                // Create a connection for each thread
-                pqxx::connection *conn = new pqxx::connection(conninfo);
-                if (!conn->is_open()) {
-                    lg.error(" Failed to connect to the database. conninfo" + conninfo);
-                } else {
-                    lg.info("Connected to the database successful.");
-                }
-                // Store the connection in thread-local storage
-                connections_thread_local.push_back(conn);
-            } catch (const std::exception &e) {
-                lg.error("Error while connecting to KingBase: " + std::string(e.what()));
-            }
-        }
     }
 }
 
